76bb2f3b7409ebd7311d5fe00f742287a6ddec91
any65889@gmail.com <ankyadav@amdocs.com>
2020-01-11 09:20:13 +0530

    Login component changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MyProfileService } from './my-profile.service';
+import { User } from './Models/user';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,23 @@ import { MyProfileService } from './my-profile.service';
 export class AppComponent {
   title = 'PhoneKart';
   textToPrint = '';
+  mainUser = '';
+  user : any;
   constructor(private myProfileService: MyProfileService) {
 
+    this.user = {
+      firstName : 'Ankit',
+      lastName : 'Yadav'
+    }
   }
 
+  ngOnInit() {
+    if(this.mainUser != ''){
+      this.mainUser = 'Login/SignUp';
+    }else{
+      this.mainUser = 'Hi ' + this.user.firstName;
+    }
+  }
   public clickMethod(){
     this.myProfileService.getMyDetails().subscribe(res =>{
       console.log(res);
