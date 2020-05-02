5971d5a6deeec3ac5143d51dbc6b7356cb772dfb
any65889@gmail.com <ankyadav@amdocs.com>
2020-05-02 16:37:13 +0530

    Latest Changes

--- src/app/Services/main.service.ts
+++ src/app/Services/main.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
@@ -10,6 +10,10 @@ export class MainService {
 
     private subject = new Subject<any>();
 
+    ngOnInit(){
+        this.fetchData();
+        this.fetchDatas();
+      }
     constructor(private http: HttpClient) { }
 
     afterAuthUrl = 'http://localhost:8765';
@@ -20,11 +24,30 @@ export class MainService {
         var headers = new HttpHeaders();
         headers = headers.set("Authorization", "Bearer " + token);
 
-
         this.http.get(this.afterAuthUrl + "/carts", { headers: headers }).subscribe(res => {
             console.log(res);
 
         }, (err) => { console.log(err) });
     }
+
+    public fetchData(){
+        const promise = this.http.get(this.afterAuthUrl).toPromise();
+
+        promise.then().then().then().catch();
+
+        promise.then(res => {
+            console.log(res)
+        }).then(rr => {
+            console.log(rr)
+        }).catch(oppp => {
+            console.log(oppp)
+        });
+
+    }
+
+    private async fetchDatas(){
+        const data = await this.http.get(this.afterAuthUrl).toPromise();
+        console.log("Data: " + JSON.stringify(data)); 
+      }
 
 }
